4ce8451f98bb02dd6f0e680d5df7556881b15371
ADMIN <ADMIN@172.16.104.53>
2024-11-08 20:57:00 +0700

    add class DAO

---
+++ src/main/java/Entity/SanPham.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SanPham {
+    private String maSP;
+    private String maLoai;
+    private String tenSP;
+    private double donGia;
+    private int soLuong;
+    private boolean trangThai;
+
+    public SanPham() {
+    }
+
+    public SanPham(String maSP, String maLoai, String tenSP, double donGia, int soLuong, boolean trangThai) {
+        this.maSP = maSP;
+        this.maLoai = maLoai;
+        this.tenSP = tenSP;
+        this.donGia = donGia;
+        this.soLuong = soLuong;
+        this.trangThai = trangThai;
+    }
+
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getMaLoai() {
+        return maLoai;
+    }
+
+    public void setMaLoai(String maLoai) {
+        this.maLoai = maLoai;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public double getDonGia() {
+        return donGia;
+    }
+
+    public void setDonGia(double donGia) {
+        this.donGia = donGia;
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
+    } 
+}
