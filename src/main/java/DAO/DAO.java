6900fb838b907a1cbaad8b047a9e169716339640
ADMIN <ADMIN@172.16.104.53>
2024-11-08 20:38:18 +0700

    add class DAO

---
+++ src/main/java/DAO/DAO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author ADMIN
+ */
+public interface DAO <Entity, Type>{
+    public List<Entity> getAllData();
+    public Entity getDataById(Type ma);
+    public void insertData(Entity o);
+    public void updateData(Entity o);
+    public void deleteById(Type ma);
+}
